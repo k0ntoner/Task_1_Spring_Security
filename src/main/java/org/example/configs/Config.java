36c904c2dcfc760bbf50a7eba948de04c8b231c7
k0ntoner <andrew4kot@gmail.com>
2024-12-27 17:18:36 +0200

    Added config class

--- src/main/java/org/example/configs/Config.java
+++ src/main/java/org/example/configs/Config.java
@@ -1,7 +1,20 @@
 package org.example.configs;
 
+import org.example.loaders.TraineeLoader;
+import org.example.models.Trainee;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
 
 @Configuration
 public class Config {
+    @Bean
+    public Map<Integer, Trainee> traineeStorage() {
+        return new TraineeLoader().load("/Users/andriikot/IdeaProjects/Epum/Task_1_Spring_Core/src/main/java/org/example/resources/trainees.json");
+    }
+
 }
