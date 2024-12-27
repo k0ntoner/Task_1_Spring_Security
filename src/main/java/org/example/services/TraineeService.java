dc04fb31106c4577b3fded3cd9995f0268d164e1
k0ntoner <andrew4kot@gmail.com>
2024-12-27 17:24:24 +0200

    Added TraineeService

--- src/main/java/org/example/services/TraineeService.java
+++ src/main/java/org/example/services/TraineeService.java
@@ -1,4 +1,32 @@
 package org.example.services;
 
+import org.example.models.Trainee;
+import org.example.DAOs.TraineeDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
 public class TraineeService {
+    private final TraineeDAO traineeDAO;
+    @Autowired
+    public TraineeService(TraineeDAO traineeDAO) {
+        this.traineeDAO = traineeDAO;
+    }
+    public void addTrainee(Trainee trainee) {
+        traineeDAO.add(trainee);
+    }
+    public Trainee getTraineeById(int id) {
+        return traineeDAO.findById(id);
+    }
+    public Map<Integer,Trainee> getAllTrainees() {
+        return  traineeDAO.findAll();
+    }
+    public void deleteTrainee(int id) {
+        traineeDAO.delete(id);
+    }
+    public void updateTrainee(Integer id, Trainee trainee) {
+        traineeDAO.update(id, trainee);
+    }
 }
