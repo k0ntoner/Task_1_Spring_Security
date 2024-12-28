c93a230aa76720fe206ca27dec3c98081e20449b
k0ntoner <andrew4kot@gmail.com>
2024-12-28 12:03:31 +0200

    Updated pom.xml

--- src/test/java/TraineeTest.java
+++ src/test/java/TraineeTest.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import org.example.configs.Config;
+import org.example.models.Trainee;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
+
 
 public class TraineeTest {
+    @Test
+    public void testTraineeAdding() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        Map<Integer, Trainee> traineeMap = (Map<Integer, Trainee>) context.getBean("traineeStorage");
+        System.out.println(traineeMap);
+    }
 }
