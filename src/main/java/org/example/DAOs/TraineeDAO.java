92f89e7fd8d54ee1e3834392948da5febdcd0800
k0ntoner <andrew4kot@gmail.com>
2024-12-28 12:05:40 +0200

    Updated TraineeDAO

--- src/main/java/org/example/DAOs/TraineeDAO.java
+++ src/main/java/org/example/DAOs/TraineeDAO.java
@@ -1,42 +1,68 @@
 package org.example.DAOs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.interfaces.DAO;
-import org.example.models.Trainee;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
+import org.example.interfaces.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
-@Repository
-public class TraineeDAO implements DAO<Trainee> {
-    private final Map<Integer, Trainee> storage;
-    @Autowired
-    public TraineeDAO(Map<Integer,Trainee> storage) {
-        this.storage = storage;
+
+public class TraineeDAO implements DAO {
+    private final Map<Integer, Model> storage;
+    private Integer head;
+    private String filePath;
+    private static final Logger logger = LoggerFactory.getLogger(TraineeDAO.class);
+    public TraineeDAO(Map<Integer,Model> traineeStorage, String traineesFilePath) {
+        this.storage = traineeStorage;
+        this.filePath = traineesFilePath;
+        logger.debug("Trainee DAO initialized: " + traineesFilePath);
+    }
+    @PostConstruct
+    public void init() {
+        head = storage.size();
+        logger.debug("Trainee DAO post initialization");
+    }
+    @PreDestroy
+    public void destroy() {
+        try {
+            logger.debug("Trainee DAO pre destroy started:");
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File(filePath), storage);
+            logger.debug("Trainee DAO pre destroy finished. File was successfully saved.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void add(Model entity) {
+        storage.put(++head,entity);
+        logger.info("Trainee DAO added new entity: {}", entity);
     }
 
     @Override
-    public void add(Trainee entity) {
-        storage.put(entity.getUserId(), entity);
+    public void update(Integer id, Model entity) {
+        storage.put(id, entity);
+        logger.info("Trainee DAO updated entity at {} to: {}",id, entity);
     }
 
     @Override
-    public void update(Integer id, Trainee entity) {
-        storage.put(entity.hashCode(), entity);
-    }
-
-    @Override
-    public Trainee findById(int id) {
+    public Model findById(int id) {
         return storage.get(id);
     }
 
     @Override
-    public Map<Integer, Trainee> findAll() {
+    public Map<Integer, Model> findAll() {
         return storage;
     }
 
     @Override
     public void delete(int id) {
         storage.remove(id);
+        logger.warn("Trainee DAO deleted entity with id: {}", id);
     }
 }
